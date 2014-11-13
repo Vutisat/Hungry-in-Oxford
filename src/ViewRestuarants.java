88938561f0c620c56818ff70c21b84762c365529
Vutisat <vutisat@miamioh.edu>
2014-11-13 10:32:41 -0500

    Made buttons functional

--- src/ViewRestuarants.java
+++ src/ViewRestuarants.java
@@ -6,27 +6,22 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class HioGui extends JPanel {
-	static JFrame frame;
-	private JButton btn1, btn2, btn3;
+public class ViewRestuarants extends JPanel {
+
+	private JButton btn1;
 	JDBC db = new JDBC();
 
-	public HioGui() {
+	public ViewRestuarants() {
 		super();
 		this.setLayout(null);
 
 		// set up the buttons
-		btn1 = new JButton("View all resturants");
+		btn1 = new JButton("Back");
 		int buttonHeight = btn1.getPreferredSize().height;
 		int buttonWidth = btn1.getPreferredSize().width;
 		btn1.setBounds(0, 50, buttonWidth, buttonHeight);
-		btn2 = new JButton("View Restuarants by food category");
-		btn2.setBounds(170, 50, buttonWidth, buttonHeight);
-		btn3 = new JButton("Back");
-		btn3.setBounds(340, 50, buttonWidth, buttonHeight);
+		
 		this.add(btn1);
-		this.add(btn2);
-		this.add(btn3);
 
 		ButtonResponder br = new ButtonResponder();
 		btn1.addActionListener(br);
@@ -40,28 +35,20 @@ public class HioGui extends JPanel {
 		public void actionPerformed(ActionEvent e) {
 
 			if (e.getSource() == btn1) {
-				// db.main(null);
+				//db.main(null);
 				System.out.println("Press 1");
-				ViewRestuarants vr = new ViewRestuarants();
-				vr.main(null);
-				vr.setVisible(true);
-
-				CloseFrame();
+				//JFrame viewR = new JFrame("View by Resturants");
 			}
 			repaint();
 		}
 
 	}
 
-	private void CloseFrame() {
-		frame.dispose();
-	}
+	public void main(String[] args) {
 
-	public static void main(String[] args) {
-
-		frame = new JFrame("Hungry In Oxford Interface");
+		JFrame frame = new JFrame("Hungry In Oxford Interface");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(new HioGui());
+		frame.add(new ViewRestuarants());
 		frame.setBounds(300, 400, 500, 500);
 		frame.setBackground(new Color(100, 0, 20));
 		frame.setLocationRelativeTo(null);
