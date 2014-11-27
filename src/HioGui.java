f36fbe96ec99be5474bb585676047223816e2a82
dazeycm <dazeycm@miamioh.edu>
2014-11-26 20:47:41 -0500

    Finished Home Page

--- src/HioGui.java
+++ src/HioGui.java
@@ -1,54 +1,76 @@
-import java.awt.Color;
-
-import java.awt.FlowLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 
 public class HioGui extends JPanel {
 	static JFrame frame;
-	private JButton btn1, btn2;
+	private JButton btn1;
 	JDBC db;
 
+	//title.setBounds(x, y, width, height);
+	
 	public HioGui() {
 		super();
-		FlowLayout experimentLayout = new FlowLayout();
-		this.setLayout(experimentLayout);
+		this.setLayout(null);
 
-		// set up the buttons
-		btn1 = new JButton("View All Resturants");
-		int buttonHeight = btn1.getPreferredSize().height;
-		int buttonWidth = btn1.getPreferredSize().width;
-		btn1.setBounds(0, 50, buttonWidth, buttonHeight);
-		btn2 = new JButton("View Restuarants by food category");
-		btn2.setSize(50, 20);
-		btn2.setBounds(170, 50, buttonWidth, buttonHeight);
+		//Title
+		JLabel title = new JLabel("Hungry in Oxford", JLabel.CENTER);
+		title.setBounds(0, 0, 400, 50);
+		title.setFont(title.getFont().deriveFont(32f));
+		this.add(title);
+		
+		//View Restaurants
+		btn1 = new JButton("View All Restaurants");
+		btn1.setBounds(75, 100, 250, 30);
 		this.add(btn1);
-		this.add(btn2);
-
 		ButtonResponder br = new ButtonResponder();
 		btn1.addActionListener(br);
+		
+		//App Description
+		String text = "NORA NORA NORA NORA NORA NORA NORA NORA NORA NORA NORA NORA NORA NORA PUT YOUR DESCRIPTION Hqwer qwer qwer qwer qwer qwer qwer qwer qwer qwer aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+		JTextPane descrip = new JTextPane();
+		 try {
+	            SimpleAttributeSet attribs = new SimpleAttributeSet();
+	            StyleConstants.setAlignment(attribs, StyleConstants.ALIGN_CENTER);
+	            descrip.setParagraphAttributes(attribs, true);
+	            descrip.setText(text);
+	        }
+	        catch (Exception ex) {
+	            ex.printStackTrace();
+	        }
+		descrip.setBounds(0, 200, 400, 300);
+		descrip.setBackground(new Color(255, 255, 255, 0));
+		descrip.setEditable(false);
+		this.add(descrip);
+		
 
-		ButtonResponder br2 = new ButtonResponder();
-		btn2.addActionListener(br2);
-		
-		this.setVisible(true); //I don't think these are necessary since we're having each class have its own jframe
-		
+		//Frame to show
 		frame = new JFrame("Hungry In Oxford");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(this);
-		frame.setBounds(300, 400, 1000, 1000);
-		frame.setBackground(new Color(100, 0, 20));
+		frame.setBounds(0, 0, 400, 400);
 		frame.setLocationRelativeTo(null);
+		frame.setPreferredSize(new Dimension(400, 400));
 		frame.setVisible(true);
 		frame.setResizable(false);
 	}
 
+
+	private void CloseFrame() {
+		frame.dispose();
+	}
+	
+
+	public static void main(String[] args) throws Exception, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		//UIManager.installLookAndFeel("SeaGlass", "com.seaglasslookandfeel.SeaGlassLookAndFeel");
+        //UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
+		HioGui start = new HioGui();
+	}
+	
 	public class ButtonResponder implements ActionListener {
 
 		@Override
@@ -59,30 +81,12 @@ public class HioGui extends JPanel {
 				ViewAllRestuarants vr = new ViewAllRestuarants();
 				//vr.main(null);
 				vr.setVisible(true);
-
 				CloseFrame();
 			}
 			
-			if (e.getSource() == btn2) {
-				ViewByCategory br = new ViewByCategory();
-				//br.main(null);
-				br.setVisible(true);
-
-				CloseFrame();
-			}
 			repaint();
 		}
 
-	}
-
-	private void CloseFrame() {
-		frame.dispose();
-	}
-
-	public static void main(String[] args) throws Exception, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-		//UIManager.installLookAndFeel("SeaGlass", "com.seaglasslookandfeel.SeaGlassLookAndFeel");
-       // UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
-		HioGui start = new HioGui();
 	}
 
 }
