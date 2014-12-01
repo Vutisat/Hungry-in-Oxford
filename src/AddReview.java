b88a8ef9f5086b5a8a1d386ad98983071da29463
norahusani <husaninm@miamioh.edu>
2014-12-01 17:15:08 -0500

    added JLabels

--- src/AddReview.java
+++ src/AddReview.java
@@ -6,43 +6,49 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.border.Border;
 
-public class AddReview extends JPanel  {
+public class AddReview extends JPanel {
 	static JFrame frame;
 	JDBC db;
 	JButton btn1;
 	JTextArea review;
 	JComboBox ratings;
 	String name;
-	
-	public AddReview(String name)	{
+
+	public AddReview(String name) {
 		super();
 		this.name = name;
 		this.setLayout(null);
-		
+
+		JLabel ratingLabel = new JLabel("Rating (1-5)");
+		ratingLabel.setBounds(20, 20, 150, 30);
+		this.add(ratingLabel);
 		// Combo box for ratings
-		String[] ratingNums = {"1", "2", "3", "4", "5"};
+		String[] ratingNums = { "1", "2", "3", "4", "5" };
 		ratings = new JComboBox(ratingNums);
-		ratings.setBounds(20, 20, 150, 30);
+		ratings.setBounds(20, 55, 150, 30);
 		this.add(ratings);
-		
-		//Submit Button
-		btn1  = new JButton("Submit");
-		btn1.setBounds(20, 75, 150, 30);
+
+		// Submit Button
+		btn1 = new JButton("Submit");
+		btn1.setBounds(20, 100, 150, 30);
 		this.add(btn1);
 		ButtonResponder br = new ButtonResponder();
 		btn1.addActionListener(br);
-		
-		//Box for review
+
+		JLabel reviewLabel = new JLabel("Enter review:");
+		reviewLabel.setBounds(200, 20, 150, 30);
+		this.add(reviewLabel);
+		// Box for review
 		review = new JTextArea();
-		review.setBounds(200, 20, 450, 225);
+		review.setBounds(200, 55, 450, 200);
 		review.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
 		this.add(review);
-		
-		//Frame to show
+
+		// Frame to show
 		frame = new JFrame(name);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(this);
@@ -51,17 +57,17 @@ public class AddReview extends JPanel  {
 		frame.setVisible(true);
 		frame.setResizable(false);
 	}
-	
+
 	private void CloseFrame() {
 		frame.dispose();
 		frame.setVisible(false);
 	}
-	
+
 	public class ButtonResponder implements ActionListener {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			if(e.getSource() == btn1)	{
+			if (e.getSource() == btn1) {
 				db = new JDBC();
 				String descrip = review.getText();
 				int num = ratings.getSelectedIndex() + 1;
